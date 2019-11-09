e3c3a2bb104eb758193458f9722bbd76f6065c4a
Balanarayanan <balan1.618@gmail.com>
2019-11-09 14:59:48 +0530

    Add styles for home

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ function App() {
       <header className="App-header">
 
       </header>
-      <p className="intro-text">
-          Hi there! I'm Balanarayanan, a frontend engineer working at Google.<br></br>
-          I'm passionate about really awesome UIs, prototyping with Arduinos and
-          mostly anything tech in general.
-      </p>
+      <div className="card">
+        <div className="about-me">
+          <img className="profile-img"
+          alt="Profile"
+          src="https://lh3.googleusercontent.com/-yQzqgKjcLhU/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rdftEvVOQQ_YBC7wVU2ur8iqFQvTw.CMID/s192-c/photo.jpg"></img>
+          <p className="intro-text">
+              Hi there! I'm Balanarayanan, a frontend engineer working at Google.<br></br><br></br>
+              I'm passionate about UIs, browser tech, prototyping with Arduinos and
+              mostly anything tech in general.
+          </p>
+        </div>
+      </div>
+
 
     </div>
   );
